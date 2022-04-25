aa0c107b6db43d922da0a2481058fc7c127e4ebb
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-04-25 16:06:41 +0000

    Completed assignment a

--- index.js
+++ index.js
@@ -13,20 +13,18 @@ function scuberGreetingForFeet(ride){
 
 function ternaryCheckCity(city){
   // Write your code here!
-  if (city === 'NYC'){
-    return 'Ok, sounds good.';
-  } else {
-    return "No go.";
-  }
+  return (city==="NYC" ? 'Ok, sounds good.' : "No go.");
 }
 
 function switchOnCharmFromTip(tip){
   // Write your code here!
-  if(tip === "generous"){
-    return "Thank you so much.";
-  } else if(tip==="not as generous"){
-    return "Thank you.";
-  } else {
-    return "Bye.";
+  switch (tip){
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return 'Bye.'
   }
 }
